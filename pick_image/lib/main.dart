0ff74377fc4bebe1652ce3f54a1d49285e4e34bd
tech-aziz <aziz.swe71@gmail.com>
2023-05-17 19:01:05 +0600

    design changes

--- pick_image/lib/main.dart
+++ pick_image/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'screens/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
@@ -12,12 +15,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage());
+    return ScreenUtilInit(
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const HomePage());
+      },
+    );
   }
 }
--- pick_image/lib/screens/home_page.dart
+++ pick_image/lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   File? images;
   Future pickImage(ImageSource source) async {
     try {
@@ -23,10 +23,11 @@ class _HomePageState extends State<HomePage> {
       print('Failed to pick image: $e');
     }
   }
+
   @override
   Widget build(BuildContext context) {
-     var screenSize = MediaQuery.of(context).size;
-     return Scaffold(
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Image Picker'),
         centerTitle: true,
@@ -37,21 +38,110 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             images != null
-                ? Container(
-                    height: 200,
-                    width: screenSize.width * 0.8,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            width: 1, color: Colors.green.withOpacity(0.5)),
-                        borderRadius: BorderRadius.circular(25)),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(25.0),
-                      child: Image.file(
-                        images!,
-                        fit: BoxFit.fill,
+                ? Stack(
+                    children: [
+                      Container(
+                        width: ScreenUtil().orientation == Orientation.portrait
+                            ? 140.w
+                            : 100.w,
+                        height: ScreenUtil().orientation == Orientation.portrait
+                            ? 140.w
+                            : 100.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(
+                              // color: ColorHelper.secondaryOrangeColor,
+                              width: 5),
+                        ),
+                        child: ClipRRect(
+                          clipBehavior: Clip.hardEdge,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(100)),
+                          child: Image.file(
+                            images!,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
                       ),
-                    ),
+                      Container(
+                        width: ScreenUtil().orientation == Orientation.portrait
+                            ? 140.w
+                            : 100.w,
+                        height: ScreenUtil().orientation == Orientation.portrait
+                            ? 140.w
+                            : 100.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(
+                              // color: ColorHelper.secondaryOrangeColor,
+                              width: 5),
+                        ),
+                        child: ClipRRect(
+                          clipBehavior: Clip.hardEdge,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(100)),
+                          child: Text(
+                            'Now image',
+                            style: TextStyle(
+                                color: Colors.blue.withOpacity(0.5),
+                                fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 100.h,
+                        left: ScreenUtil().orientation == Orientation.portrait
+                            ? 108.w
+                            : 72.w,
+                        child: Column(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                // ImagePick()
+                                //     .imagePickerOption(fromRestaurantInfo: true);
+                                // _settingsController.pickImage(false);
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(2),
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                  // color: ColorHelper.secondaryOrangeColor,
+                                  borderRadius: BorderRadius.circular(50),
+                                  border: Border.all(
+                                    width: 2,
+                                    // color: ColorHelper.secondaryOrangeColor),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.edit,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   )
+
+                // Container(
+                //     height: 200,
+                //     width: screenSize.width * 0.8,
+                //     decoration: BoxDecoration(
+                //         border: Border.all(
+                //             width: 1, color: Colors.green.withOpacity(0.5)),
+                //         borderRadius: BorderRadius.circular(25)),
+                //     child: ClipRRect(
+                //       borderRadius: BorderRadius.circular(25.0),
+                //       child: Image.file(
+                //         images!,
+                //         fit: BoxFit.fill,
+                //       ),
+                //     ),
+                //   )
+
                 : Container(
                     height: 200,
                     width: screenSize.width * 0.8,
